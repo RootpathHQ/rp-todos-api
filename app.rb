1db9de17c7cc692b4b5ed99157505cc7ec2f3ae3
Danny Smith <hi@danny.is>
2025-10-24 19:48:20 +0100

    Fix for Raleway

--- app.rb
+++ app.rb
@@ -10,6 +10,7 @@ use Rack::JSONBodyParser
 
 configure do
   set :port, ENV['PORT'] || 4567
+  set :bind, '0.0.0.0'
   set :allow_origin, :any
   set :allow_methods, %i[get post options delete put patch]
   enable :cross_origin
